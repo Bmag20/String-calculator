d181b584979e098e06ff8107bbeb76fa431e417c
Bhuvana <bhuvana.maganti@myob.com>
2021-09-07 17:33:09 +1000

    Added unit tests for step 2

--- StringCalculatorTests/UnitTest1.cs
+++ StringCalculatorTests/UnitTest1.cs
@@ -6,9 +6,15 @@ namespace StringCalculatorTests
     public class UnitTest1
     {
         [Fact]
-        public void Adding_empty_string_gives_0()
+        public void Empty_string_returns_0()
         {
             Assert.Equal(0, Calculator.Add(""));
+        }
+
+        [Fact]
+        public void A_single_number_returns_that_number()
+        {
+            Assert.Equal(3, Calculator.Add("3"));
         }
     }
 }
